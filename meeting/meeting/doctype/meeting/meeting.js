4ec280cbc107050f06c68c3fc55c744602c70fc0
Vishal <vishaldhayagude07@gmail.com>
2016-06-09 17:09:25 +0530

    First Commit

---
+++ meeting/meeting/doctype/meeting/meeting.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2016, Vishal Dhayagude and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Meeting Attendee', {
+	attendee: function(frm, cdt, cdn) {
+		var attendee = frappe.model.ger_doc(cdt , cdn);
+		if(attendee.attendee){
+			//if attendee, get full name.
+			frappe.call({
+				method: "meeting.meeting.doctype.meeting.meeting.get_full_name",
+				args: {
+					attendee:attendee.attendee
+				},
+				callback: function(r) {
+					frappe.model.set_value(cdt, cdn, "full_name", r.message);
+				}
+			});
+		}
+		else{
+			//if no attendee, clear full name
+			frappe.model.set_value(cdt, cdn, "full_name", null);
+		}
+	} 
+});
