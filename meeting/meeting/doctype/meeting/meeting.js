5eca57d531e99fb5c5733d30f7af72eb970613ee
Vishal <vishaldhayagude07@gmail.com>
2016-06-11 08:33:02 +0530

    changes in meeting.js and meeting.py and added api.py

--- meeting/meeting/doctype/meeting/meeting.js
+++ meeting/meeting/doctype/meeting/meeting.js
@@ -1,11 +1,24 @@
 // Copyright (c) 2016, Vishal Dhayagude and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Meeting Attendee', {
+frappe.ui.form.on("Meeting", {
+	send_emails: function(frm) {
+		if (frm.doc.status==="Planned") {
+			frappe.call({
+				method: "meeting.api.send_invitation_emails",
+				args: {
+					meeting: frm.doc.name
+				}
+			});
+		}
+	},
+});
+
+frappe.ui.form.on("Meeting Attendee", {
 	attendee: function(frm, cdt, cdn) {
-		var attendee = frappe.model.get_doc(cdt , cdn);
-		if(attendee.attendee){
-			//if attendee, get full name.
+		var attendee = frappe.model.get_doc(cdt, cdn);
+		if (attendee.attendee) {
+			// if attendee, get full name
 			frappe.call({
 				method: "meeting.meeting.doctype.meeting.meeting.get_full_name",
 				args: {
@@ -15,10 +28,10 @@ frappe.ui.form.on('Meeting Attendee', {
 					frappe.model.set_value(cdt, cdn, "full_name", r.message);
 				}
 			});
-		}
-		else{
-			//if no attendee, clear full name
+
+		} else {
+			// if no attendee, clear full name
 			frappe.model.set_value(cdt, cdn, "full_name", null);
 		}
-	} 
+ 	},
 });
--- meeting/meeting/doctype/meeting/meeting.py
+++ meeting/meeting/doctype/meeting/meeting.py
@@ -10,14 +10,13 @@ from frappe.model.document import Document
 class Meeting(Document):
 	def validate(self):
 		"""Set missing names and warn if duplicate"""
-		found =[]
+		found = []
 		for attendee in self.attendees:
 			if not attendee.full_name:
 				attendee.full_name = get_full_name(attendee.attendee)
-			
+
 			if attendee.attendee in found:
 				frappe.throw(_("Attendee {0} entered twice").format(attendee.attendee))
-
 			found.append(attendee.attendee)
 
 
